f9706a3d71a3fafab6449b97347136499626024b
jenkins <jenkins-server@spotify.com>
2016-02-26 15:48:36 +0000

    Update helios to 0.8.741

--- helios-solo.rb
+++ helios-solo.rb
@@ -2,11 +2,11 @@ require "formula"
 
 class HeliosSolo < Formula
   homepage "https://github.com/spotify/helios"
-  url "https://github.com/spotify/helios/releases/download/0.8.739/helios-solo.zip"
-  sha1 "f62ec1b65d17cdb4ed2f41c8899c49d93f3bb545"
-  version "0.8.739"
+  url "https://github.com/spotify/helios/releases/download/0.8.741/helios-solo.zip"
+  sha1 "744daf8555311149aa2291a41a486f5f4fdeada3"
+  version "0.8.741"
 
-  depends_on "spotify/public/helios" => "0.8.739"
+  depends_on "spotify/public/helios" => "0.8.741"
   depends_on "jq"
 
   def install
--- helios.rb
+++ helios.rb
@@ -2,15 +2,15 @@ require "formula"
 
 class Helios < Formula
   homepage "https://github.com/spotify/helios"
-  url "https://oss.sonatype.org/service/local/repositories/releases/content/com/spotify/helios-tools/0.8.739/helios-tools-0.8.739-shaded.jar"
-  sha1 "2023a63e1b1db8ee03dd9b669d358900ab769f71"
-  version "0.8.739"
+  url "https://oss.sonatype.org/service/local/repositories/releases/content/com/spotify/helios-tools/0.8.741/helios-tools-0.8.741-shaded.jar"
+  sha1 "5d668709280ab6f14ad9462bd9d2d30fa970f6f3"
+  version "0.8.741"
 
   depends_on :java => "1.7+"
 
   def install
-    libexec.install "helios-tools-0.8.739-shaded.jar"
-    bin.write_jar_script libexec/"helios-tools-0.8.739-shaded.jar", "helios", "-XX:+TieredCompilation -XX:TieredStopAtLevel=1 -Xverify:none"
+    libexec.install "helios-tools-0.8.741-shaded.jar"
+    bin.write_jar_script libexec/"helios-tools-0.8.741-shaded.jar", "helios", "-XX:+TieredCompilation -XX:TieredStopAtLevel=1 -Xverify:none"
   end
 
   test do

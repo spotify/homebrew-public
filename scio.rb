9a3510567e27a3ac5722aff3edabb955ff90a97f
Neville Li <neville.lyh@gmail.com>
2016-08-29 08:21:50 -0700

    scio 0.2.0 (new formula) (#8)

---
+++ scio.rb
@@ -0,0 +1,30 @@
+class Scio < Formula
+  desc "Scala API for Google Cloud Dataflow and Apache Beam"
+  homepage "https://github.com/spotify/scio/"
+  url "https://github.com/spotify/scio/releases/download/v0.2.0/scio-repl-0.2.0.jar"
+  sha256 "220e03dfd866d8475fc7943f60376ea4fe0862c3520674e46ef8262a09d1756c"
+
+  bottle :unneeded
+
+  def install
+    libexec.install "scio-repl-#{version}.jar"
+    (bin+"scio-repl").write <<-EOS.undent
+      #!/bin/bash
+      java_opts=()
+      scio_opts=()
+      for o in "$@"; do
+          if [[ "$o" =~ ^-D.* ]]; then
+              java_opts+=("$o")
+          else
+              scio_opts+=("$o")
+          fi
+      done
+
+      exec java ${java_opts[*]} -jar #{libexec}/"scio-repl-#{version}.jar" ${scio_opts[*]}
+    EOS
+  end
+
+  test do
+    shell_output("#{bin}/scio-repl -X", 1)
+  end
+end

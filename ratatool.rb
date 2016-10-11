68f4c808cfa7597052609b2310a6669b766c41cb
Neville Li <neville.lyh@gmail.com>
2016-10-11 19:25:33 -0400

    Update ratatool to 0.1.8

--- ratatool.rb
+++ ratatool.rb
@@ -1,8 +1,8 @@
 class Ratatool < Formula
   desc "Tool for random data sampling and generation"
   homepage "https://github.com/spotify/ratatool"
-  url "https://github.com/spotify/ratatool/releases/download/v0.1.4/ratatool-0.1.4.jar"
-  sha256 "90e9da5ac9f2d7ff96141fc5ca295b5882896501ac6d656d9211992d5541314c"
+  url "https://github.com/spotify/ratatool/releases/download/v0.1.8/ratatool-0.1.8.jar"
+  sha256 "a74ad263bc7b232500d15bee7a0e6a54669b028d9bbd9f88db53550f3a1f40b3"
 
   bottle :unneeded
 

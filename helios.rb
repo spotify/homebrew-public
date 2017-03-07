296d03f54f4201e1a01bde3c545ff7412ad36315
Staffan Gimåker <staffan@spotify.com>
2017-03-07 09:36:05 -0500

    Update helios to 0.9.104

--- helios.rb
+++ helios.rb
@@ -3,7 +3,7 @@ require "formula"
 class Helios < Formula
   homepage "https://github.com/spotify/helios"
   url "https://oss.sonatype.org/service/local/repositories/releases/content/com/spotify/helios-tools/0.9.104/helios-tools-0.9.104-shaded.jar"
-  sha256 "539f16853d21bd82afe0de82bb34c09b5cfcd9033c692c159abaa63f7ffcaaae"
+  sha256 "ff7acd2d724ec8d93cd750a13258d89e0c799dd659bf6647d52e126d6465811e"
   version "0.9.104"
 
   depends_on :java => "1.7+"

b411962a2158ed947fa57b72f88691ffbc00683e
jenkins <jenkins-server@spotify.com>
2017-05-03 20:16:31 +0000

    Update helios to 0.9.128

--- helios.rb
+++ helios.rb
@@ -3,7 +3,7 @@ require "formula"
 class Helios < Formula
   homepage "https://github.com/spotify/helios"
   url "https://oss.sonatype.org/service/local/repositories/releases/content/com/spotify/helios-tools/0.9.128/helios-tools-0.9.128-shaded.jar"
-  sha256 "2977c66b07745f431d979bfbc04f57267cb9760400f9771897655fea0e12111e"
+  sha256 "dd2e7aa482769a6f81c889e3a29ba617e585f279a649ce00be8d09c52254076c"
   version "0.9.128"
 
   depends_on :java => "1.7+"

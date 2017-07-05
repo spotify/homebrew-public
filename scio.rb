035cde6275ba69cc02b3f73d2968ddd395e0f84b
Neville Li <neville.lyh@gmail.com>
2017-07-05 13:58:34 -0400

    Update scio to 0.3.3

--- scio.rb
+++ scio.rb
@@ -1,8 +1,8 @@
 class Scio < Formula
   desc "Scala API for Google Cloud Dataflow and Apache Beam"
   homepage "https://github.com/spotify/scio/"
-  url "https://github.com/spotify/scio/releases/download/v0.3.2/scio-repl-0.3.2.jar"
-  sha256 "dff54d7b926b91cdd7de7ce580d4deef4b893c48d1f12967c0177320258df42c"
+  url "https://github.com/spotify/scio/releases/download/v0.3.3/scio-repl-0.3.3.jar"
+  sha256 "e48635537208b8bf8d740ec5a5f95f3b3a18ba04fd48944dc8b5de518b7f5f57"
 
   bottle :unneeded
 

b30fb1e73572ee602dd238b5d8dc2b519629ece1
jenkins <jenkins-server@spotify.com>
2017-09-13 21:59:37 +0000

    Update helios to 0.9.154

--- helios.rb
+++ helios.rb
@@ -2,15 +2,15 @@ require "formula"
 
 class Helios < Formula
   homepage "https://github.com/spotify/helios"
-  url "https://oss.sonatype.org/service/local/repositories/releases/content/com/spotify/helios-tools/0.9.153/helios-tools-0.9.153-shaded.jar"
-  sha256 "a2e9ba68d726827cdbe9b94cd9319b44202c3702ab5b443ecaa31187ac7ba987"
-  version "0.9.153"
+  url "https://oss.sonatype.org/service/local/repositories/releases/content/com/spotify/helios-tools/0.9.154/helios-tools-0.9.154-shaded.jar"
+  sha256 "5b2dfa659c6e46de8c0e1f837c4cd82545c6a1f95f329a9f5ea827ce1ea98d4c"
+  version "0.9.154"
 
   depends_on :java => "1.7+"
 
   def install
-    libexec.install "helios-tools-0.9.153-shaded.jar"
-    bin.write_jar_script libexec/"helios-tools-0.9.153-shaded.jar", "helios", "-XX:+TieredCompilation -XX:TieredStopAtLevel=1 -Xverify:none"
+    libexec.install "helios-tools-0.9.154-shaded.jar"
+    bin.write_jar_script libexec/"helios-tools-0.9.154-shaded.jar", "helios", "-XX:+TieredCompilation -XX:TieredStopAtLevel=1 -Xverify:none"
   end
 
   test do

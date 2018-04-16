75fe780f21bdc196004d2a7326cee5bd41e66c12
Idrees Khan <idrees@spotify.com>
2018-04-16 14:38:30 -0400

    Move ratatool to SBT pack formula

--- ratatool.rb
+++ ratatool.rb
@@ -1,17 +1,17 @@
 class Ratatool < Formula
   desc "Tool for random data sampling and generation"
   homepage "https://github.com/spotify/ratatool"
-  url "https://github.com/spotify/ratatool/releases/download/v0.3.1/ratatool-0.3.1.jar"
-  sha256 "45d017c5eb019790a90dcc7cb3a3c2762219eaa5719ac93988ac7d4678500e47"
-
-  bottle :unneeded
+  url "https://github.com/spotify/ratatool/releases/download/v0.3.2-SNAPSHOT/ratatool-0.3.2-SNAPSHOT.tar.gz"
+  sha256 "7684a016e3619e1860cfbb08ae0a3932b5c42d2c8d698927fc9d6fe59be083fb"
 
   def install
-    libexec.install "ratatool-#{version}.jar"
-    bin.write_jar_script libexec/"ratatool-#{version}.jar", "ratatool"
+      lib.install Dir["lib/*.jar"]
+      bin.install "bin/big-diffy"
+      bin.install "bin/big-sampler"
+      bin.install "bin/tool"
   end
 
   test do
-    system "#{bin}/ratatool"
+    system "#{bin}/tool"
   end
 end

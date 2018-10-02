8b87ab91c009fd1e3389d67ee299c62d097ea495
Idrees Khan <idrees@spotify.com>
2018-10-02 16:57:21 -0400

    Bump Ratatool to 0.3.6

--- ratatool.rb
+++ ratatool.rb
@@ -1,8 +1,8 @@
 class Ratatool < Formula
   desc "Tool for random data sampling and generation"
   homepage "https://github.com/spotify/ratatool"
-  url "https://github.com/spotify/ratatool/releases/download/v0.3.5/ratatool-cli-0.3.5.tar.gz"
-  sha256 "404ecb682f7ea142b7aa6f8ad7a032d2816d12420063902c92b12f67c5cc1619"
+  url "https://github.com/spotify/ratatool/releases/download/v0.3.6/ratatool-cli-0.3.6.tar.gz"
+  sha256 "e5623938957e3da383439e7beec0b039200f392fcade074184fc649694ec6e9b"
 
   def install
       lib.install Dir["lib/*.jar"]

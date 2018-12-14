99a81cf889b5229b759092429c68f35456f9fc1d
jenkins <jenkins-server@spotify.com>
2018-12-14 20:00:28 +0000

    Update helios to 0.9.235

--- helios.rb
+++ helios.rb
@@ -2,9 +2,9 @@ require "formula"
 
 class Helios < Formula
   homepage "https://github.com/spotify/helios"
-  url "https://oss.sonatype.org/service/local/repositories/releases/content/com/spotify/helios-tools/0.9.234/helios-tools-0.9.234-shaded.jar"
-  sha256 "ca345a1331981235a8b398812841f26b67dc69238756162d7c82a6929eee721b"
-  version "0.9.234"
+  url "https://oss.sonatype.org/service/local/repositories/releases/content/com/spotify/helios-tools/0.9.235/helios-tools-0.9.235-shaded.jar"
+  sha256 "6cff33d03c48cc04c23c73ae3c901ca5c1fe037f8d0754f7f2ec096fdde1ae98"
+  version "0.9.235"
 
   depends_on :java => "1.7+"
 

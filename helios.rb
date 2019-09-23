77a8f28a64a4b53ec2c7a00e207d736b93ae6d64
spotify-helios-ci-agent <dxia+spotify-helios-ci-agent@spotify.com>
2019-09-23 14:16:54 +0000

    Update helios to 0.9.262

--- helios.rb
+++ helios.rb
@@ -2,9 +2,9 @@ require "formula"
 
 class Helios < Formula
   homepage "https://github.com/spotify/helios"
-  url "https://oss.sonatype.org/service/local/repositories/releases/content/com/spotify/helios-tools/0.9.261/helios-tools-0.9.261-shaded.jar"
-  sha256 "e5bd27d0c0c64a2f284af14c753695a106d90151b6c879e3e6e1aafe36e1e2f9"
-  version "0.9.261"
+  url "https://oss.sonatype.org/service/local/repositories/releases/content/com/spotify/helios-tools/0.9.262/helios-tools-0.9.262-shaded.jar"
+  sha256 "82d6367452e6b9f6540217d1c25ce66fc03cdca6af1de1724e2d0a3176bcc0f7"
+  version "0.9.262"
 
   depends_on :java => "1.7+"
 

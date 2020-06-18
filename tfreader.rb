90b5a0fdf8a070a0067f66a6c53004e33e050659
Filipe Regadas <filiperegadas@gmail.com>
2020-06-18 16:51:28 +0100

    Update tfreader to 0.0.5 (#59)

--- tfreader.rb
+++ tfreader.rb
@@ -1,7 +1,7 @@
 class Tfreader < Formula
   homepage "https://github.com/spotify/tfreader"
-  url "https://github.com/spotify/tfreader/releases/download/v0.0.4/tfr-v0.0.4-macOS.tar.gz"
-  sha256 "25ab8c5c9736e20ca6432d7ef46cadaf16374f919ff2477c7d9a77b5ff718f53"
+  url "https://github.com/spotify/tfreader/releases/download/v0.0.5/tfr-v0.0.5-macOS.tar.gz"
+  sha256 "1c1703338ed7975a538279ce25133cb0cc001218068a8cee3f11b4270fd0e837"
 
   def install
     bin.install "tfr"

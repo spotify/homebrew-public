38750fea7d53e93ef17f6577db6c29ebb680fac9
Neville Li <neville@spotify.com>
2020-07-01 15:43:57 -0400

    update gcs-tools to 0.1.8 (#60)

--- gcs-avro-tools.rb
+++ gcs-avro-tools.rb
@@ -1,15 +1,15 @@
 class GcsAvroTools < Formula
   desc "GCS compatible Apache Avro Tools"
   homepage "https://github.com/spotify/gcs-tools"
-  url "https://github.com/spotify/gcs-tools/releases/download/v0.1.7/avro-tools-1.8.2.jar"
-  sha256 "d251d591b5cdd8883cc2e5dfc1ede582a2baf127e7e2634d6a0464edca1a2f4d"
-  version "0.1.7"
+  url "https://github.com/spotify/gcs-tools/releases/download/v0.1.8/avro-tools-1.8.2.jar"
+  sha256 "f37388a9268f26a8565aab23ec5ccf0bfd12047fa18d0c1a2f1ecd9815c8b1bb"
+  version "0.1.8"
 
   conflicts_with "avro-tools", :because => "provides the same binaries/links."
 
   bottle :unneeded
 
-  depends_on :java => "1.7+"
+  depends_on :java => "1.8+"
 
   def install
     libexec.install "avro-tools-1.8.2.jar"
--- gcs-parquet-tools.rb
+++ gcs-parquet-tools.rb
@@ -1,19 +1,19 @@
 class GcsParquetTools < Formula
   desc "GCS compatible Apache Parquet Tools"
   homepage "https://github.com/spotify/gcs-tools"
-  url "https://github.com/spotify/gcs-tools/releases/download/v0.1.7/parquet-tools-1.9.0.jar"
-  sha256 "2519d6198f0fec7cb2748b3c3abb5b83a5c32d0b3c6167ba5f82913d8b5dd80c"
-  version "0.1.7"
+  url "https://github.com/spotify/gcs-tools/releases/download/v0.1.8/parquet-tools-1.11.0.jar"
+  sha256 "c9b147b134d3a0d0be0abf38be22481a48a813fbba2c844b1e0f1e396aa25c0c"
+  version "0.1.8"
 
   conflicts_with "parquet-tools", :because => "provides the same binaries/links."
 
   bottle :unneeded
 
-  depends_on :java => "1.7+"
+  depends_on :java => "1.8+"
 
   def install
-    libexec.install "parquet-tools-1.9.0.jar"
-    bin.write_jar_script libexec/"parquet-tools-1.9.0.jar", "parquet-tools"
+    libexec.install "parquet-tools-1.11.0.jar"
+    bin.write_jar_script libexec/"parquet-tools-1.11.0.jar", "parquet-tools"
   end
 
   test do

d13b2efb670a4d2f5b0b85bcf50170e918efca06
Neville Li <neville.lyh@gmail.com>
2020-08-03 11:15:17 -0400

    update gcs-tools to 0.1.10

--- gcs-parquet-tools.rb
+++ gcs-parquet-tools.rb
@@ -1,9 +1,9 @@
 class GcsParquetTools < Formula
   desc "GCS compatible Apache Parquet Tools"
   homepage "https://github.com/spotify/gcs-tools"
-  url "https://github.com/spotify/gcs-tools/releases/download/v0.1.9/parquet-tools-1.11.0.jar"
-  sha256 "0c5d9a597fa4ab42d1d01e953d92ded7de3d46e473d74b5562721010520ab186"
-  version "0.1.9"
+  url "https://github.com/spotify/gcs-tools/releases/download/v0.1.10/parquet-tools-1.11.0.jar"
+  sha256 "3b129e6c888530c30c9ca5d86d31129b4c8a4385310553d74a3e91cf286b695b"
+  version "0.1.10"
 
   conflicts_with "parquet-tools", :because => "provides the same binaries/links."
 

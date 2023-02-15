c3342bf9b6f651f4716908fafe0fb32c2db78552
Idrees Khan <idrees@spotify.com>
2023-02-15 15:11:23 -0500

    Upgrade ratatool to 0.4.1

--- ratatool.rb
+++ ratatool.rb
@@ -1,8 +1,8 @@
 class Ratatool < Formula
   desc "Tool for random data sampling and generation"
   homepage "https://github.com/spotify/ratatool"
-  url "https://github.com/spotify/ratatool/releases/download/v0.4.0/ratatool-cli-0.4.0.tar.gz"
-  sha256 "d99a22232cd294aa2f101aef466676bfaa432ba8ead8940475856f623b7d8db9"
+  url "https://github.com/spotify/ratatool/releases/download/v0.4.1/ratatool-cli-0.4.1.tar.gz"
+  sha256 "50d66dbfc1e8a85fbad07aa6ef81c838c9b87a4e7d1d9030d9d5acc643f661c1"
 
   def install
       lib.install Dir["lib/*.jar"]

34616da1cfff7287f73e4fb3fb0fe14f9a7345c3
Michel Davit <michel@davit.fr>
2023-05-03 12:55:56 +0200

    Bump gcs-tools to 0.2.4 (#103)

--- gcs-magnolify-tools.rb
+++ gcs-magnolify-tools.rb
@@ -1,9 +1,9 @@
 class GcsMagnolifyTools < Formula
   desc "GCS compatible Magnolify Tools"
   homepage "https://github.com/spotify/gcs-tools"
-  url "https://github.com/spotify/gcs-tools/releases/download/v0.2.3/magnolify-tools-0.6.2.jar"
-  sha256 "7786e24710578a952d158e011c7e59c0a5b9922fee4eede791e124fc2ce8f690"
-  version "0.2.3"
+  url "https://github.com/spotify/gcs-tools/releases/download/v0.2.4/magnolify-tools-0.6.2.jar"
+  sha256 "7333c51fb8767613cbb484041a7da58058bf53b207a738d7a103b5387969ddff"
+  version "0.2.4"
 
   def install
     libexec.install "magnolify-tools-0.6.2.jar"
--- gcs-parquet-cli.rb
+++ gcs-parquet-cli.rb
@@ -1,13 +1,13 @@
 class GcsParquetCli < Formula
   desc "GCS compatible Apache Parquet CLI"
   homepage "https://github.com/spotify/gcs-tools"
-  url "https://github.com/spotify/gcs-tools/releases/download/v0.2.3/parquet-cli-1.12.3.jar"
-  sha256 "0c110f432567f0e663c56402ebec70f723cd21924847ba906d765efb4ed2ae12"
-  version "0.2.3"
+  url "https://github.com/spotify/gcs-tools/releases/download/v0.2.4/parquet-cli-1.13.0.jar"
+  sha256 "4e98e40db5eca870d172572e39b720233c6aaf71c9e3d9453a29691d412b9736"
+  version "0.2.4"
 
   def install
-    libexec.install "parquet-cli-1.12.3.jar"
-    bin.write_jar_script libexec/"parquet-cli-1.12.3.jar", "parquet-cli"
+    libexec.install "parquet-cli-1.13.0.jar"
+    bin.write_jar_script libexec/"parquet-cli-1.13.0.jar", "parquet-cli"
   end
 
   test do

379d0997830124c8f7e63dab386e83608e5b68c8
Michel Davit <michel@davit.fr>
2023-05-03 12:54:49 +0200

    Bump gcs-tools to 0.2.4

--- gcs-avro-tools.rb
+++ gcs-avro-tools.rb
@@ -1,9 +1,9 @@
 class GcsAvroTools < Formula
   desc "GCS compatible Apache Avro Tools"
   homepage "https://github.com/spotify/gcs-tools"
-  url "https://github.com/spotify/gcs-tools/releases/download/v0.2.3/avro-tools-1.11.1.jar"
-  sha256 "3d015ba2e91eabc672b6ee560afa4aded1a74bb1fbfb6ba0e504aa6bd76ca0d2"
-  version "0.2.3"
+  url "https://github.com/spotify/gcs-tools/releases/download/v0.2.4/avro-tools-1.11.1.jar"
+  sha256 "ce80e93a1e9b3affb2012fa22547492472665e09360fb63378085064822e7af1"
+  version "0.2.4"
 
   conflicts_with "avro-tools", :because => "provides the same binaries/links."
 
--- gcs-proto-tools.rb
+++ gcs-proto-tools.rb
@@ -1,13 +1,13 @@
 class GcsProtoTools < Formula
   desc "GCS compatible tools for Scio Protobuf in Avro file"
   homepage "https://github.com/spotify/gcs-tools"
-  url "https://github.com/spotify/gcs-tools/releases/download/v0.2.3/proto-tools-3.21.9.jar"
-  sha256 "531c50cdfd228b760b33c4dfae4c1d3f75a04f10de06251b8d7801277bd6c2fe"
-  version "0.2.3"
+  url "https://github.com/spotify/gcs-tools/releases/download/v0.2.4/proto-tools-3.22.3.jar"
+  sha256 "ce9bec0f4c2658eef10f081e5d3f56eb25f54808cb777189ba2523f3068faf28"
+  version "0.2.4"
 
   def install
-    libexec.install "proto-tools-3.21.9.jar"
-    bin.write_jar_script libexec/"proto-tools-3.21.9.jar", "proto-tools"
+    libexec.install "proto-tools-3.22.3.jar"
+    bin.write_jar_script libexec/"proto-tools-3.22.3.jar", "proto-tools"
   end
 
   test do

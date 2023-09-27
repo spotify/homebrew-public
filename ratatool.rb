e9e0511a8aaf26c9be831940666b36d3466caee7
Ben Konz <benkonz16@gmail.com>
2023-09-27 13:40:11 -0400

    fix ratatool 0.4.2 sha (#106)

--- ratatool.rb
+++ ratatool.rb
@@ -2,7 +2,7 @@ class Ratatool < Formula
   desc "Tool for random data sampling and generation"
   homepage "https://github.com/spotify/ratatool"
   url "https://github.com/spotify/ratatool/releases/download/v0.4.2/ratatool-cli-0.4.2.tar.gz"
-  sha256 "f9f5038d9dea6dc7215d0487b0b57acbb493e984812f294b0bd57c955799fe37"
+  sha256 "cf970ba72318c727d8efe94ec0876760e1ce7f2440dbd2c2eec9703f533806c7"
 
   def install
       lib.install Dir["lib/*.jar"]

935d95ac0bdd991339d715dda252adde7985af74
Catherine Elder <siege@spotify.com>
2023-11-27 10:53:33 -0500

    Update ratatool.rb

--- ratatool.rb
+++ ratatool.rb
@@ -1,8 +1,8 @@
 class Ratatool < Formula
   desc "Tool for random data sampling and generation"
   homepage "https://github.com/spotify/ratatool"
-  url "https://github.com/spotify/ratatool/releases/download/v0.4.2/ratatool-cli-0.4.2.tar.gz"
-  sha256 "cf970ba72318c727d8efe94ec0876760e1ce7f2440dbd2c2eec9703f533806c7"
+  url "https://github.com/spotify/ratatool/releases/download/v0.4.3/ratatool-cli-0.4.3.tar.gz"
+  sha256 "1970ed2b4787f6c759fc9c739f7183e3874157a5324edcd1e3db21b124fea328"
 
   def install
       lib.install Dir["lib/*.jar"]

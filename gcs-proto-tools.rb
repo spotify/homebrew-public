28c72b225c176c5ef8bfb63441fb6a4706e79aef
Michel Davit <michel@davit.fr>
2024-01-19 10:18:34 +0100

    Bump gcs-tools to 0.3.1

--- gcs-proto-tools.rb
+++ gcs-proto-tools.rb
@@ -1,13 +1,13 @@
 class GcsProtoTools < Formula
   desc "GCS compatible tools for Scio Protobuf in Avro file"
   homepage "https://github.com/spotify/gcs-tools"
-  url "https://github.com/spotify/gcs-tools/releases/download/v0.3.0/proto-tools-3.24.4.jar"
-  sha256 "2ebe07ffbd2ec296a15481377556f17a5acae2ef8f189b8e9c7950d266897e63"
-  version "0.3.0"
+  url "https://github.com/spotify/gcs-tools/releases/download/v0.3.1/proto-tools-3.25.2.jar"
+  sha256 "01ba3a3fa46bd884f60e4c6fd00c4eda288d5ce6e975906f332339315a3dc457"
+  version "0.3.1"
 
   def install
-    libexec.install "proto-tools-3.24.4.jar"
-    bin.write_jar_script libexec/"proto-tools-3.24.4.jar", "proto-tools"
+    libexec.install "proto-tools-3.25.2.jar"
+    bin.write_jar_script libexec/"proto-tools-3.25.2.jar", "proto-tools"
   end
 
   test do

6897af6a5dfd7da825c967ab9b13dafaa3a5040d
Monica Zamudio Lopez <monicaz@spotify.com>
2024-03-20 18:18:04 -0400

    Ratatool 0.4.8

--- ratatool.rb
+++ ratatool.rb
@@ -1,8 +1,8 @@
 class Ratatool < Formula
   desc "Tool for random data sampling and generation"
   homepage "https://github.com/spotify/ratatool"
-  url "https://github.com/spotify/ratatool/releases/download/v0.4.7/ratatool-cli-0.4.7.tar.gz"
-  sha256 "7a6653a03254eda9c0a0e127c35db98f6f6ec26309682ee8cb23ac71143b7993"
+  url "https://github.com/spotify/ratatool/releases/download/v0.4.8/ratatool-cli-0.4.8.tar.gz"
+  sha256 "24d569bd517f35d724ab5271cf868f659f294a1097ea62a6ad8ff884c65520ac"
 
   def install
       lib.install Dir["lib/*.jar"]

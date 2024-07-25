3f132d8264b358538a48741cf02ee3f170ff2a1a
Michel Davit <michel@davit.fr>
2024-07-25 15:04:39 +0200

    Bump Scio to 0.14.6

--- scio.rb
+++ scio.rb
@@ -1,8 +1,8 @@
 class Scio < Formula
   desc "Scala API for Apache Beam and Google Cloud Dataflow"
   homepage "https://github.com/spotify/scio/"
-  url "https://github.com/spotify/scio/releases/download/v0.14.5/scio-repl.jar"
-  sha256 "92c5e616b82e9210fdb33b2c8fcb1170a6f253b7f8d997b164c0ebc275ef5812"
+  url "https://github.com/spotify/scio/releases/download/v0.14.6/scio-repl.jar"
+  sha256 "3a24b73cfbcad404cb9116ce5090f196d721943e4373c758fdc4fb124e9df46a"
 
   def install
     libexec.install "scio-repl.jar"

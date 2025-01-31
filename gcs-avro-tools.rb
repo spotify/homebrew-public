a36382e6361810e57d0ec3fd4306fd7f5300ca0b
Michel Davit <michel@davit.fr>
2025-01-31 23:39:11 +0100

    Bump gcs-tools to 0.3.4 (#129)

--- gcs-avro-tools.rb
+++ gcs-avro-tools.rb
@@ -1,9 +1,9 @@
 class GcsAvroTools < Formula
   desc "GCS compatible Apache Avro Tools"
   homepage "https://github.com/spotify/gcs-tools"
-  url "https://github.com/spotify/gcs-tools/releases/download/v0.3.3/avro-tools-1.12.0.jar"
-  sha256 "e8645343fc108cd855c817f89afa58a595cf9365ae8dbdf85abff6d0a45b5b91"
-  version "0.3.3"
+  url "https://github.com/spotify/gcs-tools/releases/download/v0.3.4/avro-tools-1.12.0.jar"
+  sha256 "61f16128e27cf0def954c6f622e1d2241d38209c51c9be517517303fa9aa4d5d"
+  version "0.3.4"
 
   conflicts_with "avro-tools", :because => "provides the same binaries/links."
 
